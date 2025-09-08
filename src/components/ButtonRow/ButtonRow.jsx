87459ba8a85e62c6e81916cac67fe96f51d474df
Kimberly Dauber <noteflightthemes@gmail.com>
2025-09-08 11:52:34 -0400

    button bar horizontal scrollbars - exercise 3

--- src/components/ButtonRow/ButtonRow.jsx
+++ src/components/ButtonRow/ButtonRow.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import styles from './ButtonRow.module.css';
 
 const ButtonRow = ({ children }) => {
-  return <div className={styles.buttonRow}>{children}</div>;
+  return <div className={styles.buttonRow}>
+    <div className={styles.buttonRowWidthWrapper}>{children}</div>
+  </div>;
 };
 
 export default ButtonRow;
